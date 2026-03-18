46c455bb9daf58c8aa1eb96efbddcffac2d7898f
IrinaSantos <irina.santos.pt@gmail.com>
2026-03-18 14:53:31 +0000

    adionar permissions à casa

--- model/Casa.java
+++ model/Casa.java
@@ -3,12 +3,15 @@ package model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Casa implements Serializable {
 
     private String name;
     private String owner;
     private List<Device> devices = new ArrayList<>();
+    private Map<String, List<String>> permissions = new HashMap<>();
 
     public Casa(String name, String owner) {
         this.name = name;
@@ -17,6 +20,11 @@ public class Casa implements Serializable {
 
     public void addDevice(Device device) {
         devices.add(device);
+    }
+
+    public void addPermission(String username, String section) {
+        permissions.putIfAbsent(username, new ArrayList<>());
+        permissions.get(username).add(section);
     }
 
     public String getName() {
@@ -31,4 +39,7 @@ public class Casa implements Serializable {
         return devices;
     }
 
+    public Map<String, List<String>> getPermissions() {
+        return permissions;
+    }
 }
